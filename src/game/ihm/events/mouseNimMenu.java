914ff369db062c3c177909c28449e72a939db3c4
Sodoxs <guillaume.garcia020@gmail.com>
2019-01-18 16:07:38 +0100

    nouveau fichier : src/game/ihm/events/mouseNimMenu.java

---
+++ src/game/ihm/events/mouseNimMenu.java
@@ -0,0 +1,44 @@
+package game.ihm.events;
+
+import game.ihm.graphic.NimGame;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+/**
+ *
+ * @author Guillaume and Florian
+ */
+public class mouseNimMenu implements MouseListener
+{
+    private NimGame ng;
+    public mouseNimMenu(NimGame ng)
+    {
+        this.ng = ng;
+    }
+    @Override
+    public void mouseClicked(MouseEvent ae) 
+    {
+       
+    }
+
+    @Override
+    public void mousePressed(MouseEvent me) 
+    {
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent me) 
+    {
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent me) 
+    {
+    }
+
+    @Override
+    public void mouseExited(MouseEvent me) 
+    {
+    }
+    
+}
